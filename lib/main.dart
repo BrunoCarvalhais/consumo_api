b5e0ea1900f0c31a8f8fa3f5a2d5fef2527e43fe
BrunoCarvalhais <carvalhaisazevedo@gmail.com>
2021-10-21 15:48:00 -0300

    commit1

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consumo_api/Home.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
